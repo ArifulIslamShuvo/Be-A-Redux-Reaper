e24998f94f35a87219d2b2f23db4bc278cc7260d
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-07-11 20:43:55 +0600

    [Done]File Structure and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import routers from './routes/index.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+       <RouterProvider router={routers} />
   </React.StrictMode>,
 )
